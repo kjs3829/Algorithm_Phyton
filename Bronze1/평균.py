4edce4922502c0f12bed421a8810130ae6815b19
JSK <kjs35323@naver.com>
2021-01-05 20:26:24 +0900

    Day-10

---
+++ Bronze1/평균.py
@@ -0,0 +1,16 @@
+# Baekjoon 1546 평균
+
+a = int(input())
+b = list(map(int, input().split()))
+c = 0
+new_point = []
+
+for i in b:
+    c += i
+
+avg_old = c / a
+
+for i in b:
+    new_point.append(i / max(b) * 100)
+
+print("%0.2f" % (sum(new_point) / a))
