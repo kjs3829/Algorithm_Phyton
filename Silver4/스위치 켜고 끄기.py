2b726dd910453a4efcbe878ea1f320ada9e30d1b
JunSeong <kjs35323@naver.com>
2021-02-06 22:44:40 +0900

    Day-27 fix

--- Silver4/스위치 켜고 끄기.py
+++ Silver4/스위치 켜고 끄기.py
@@ -1,13 +1,34 @@
-# Baekjoon 2217 로프
+# BOJ 1244 스위치 켜고 끄기
+from sys import stdin
+SPLITS = 20
 
-import sys
+def find_symmetric(arr, idx):
+    r, l = idx - 1, idx + 1
+    min_r, max_l = idx, idx
+    while r > 0 and l < len(arr):
+        if arr[r] == arr[l]:
+            min_r, max_l = r, l
+            r -= 1
+            l += 1
+        else:
+            break
+    return max(idx - min_r, max_l - idx)
 
-N = int(sys.stdin.readline())
-rope = []
-value = []
-for i in range(N):
-    rope.append(int(sys.stdin.readline()))
-rope.sort(reverse=True)
-for num in range(N):
-    value.append(rope[num]*(num+1))
-print(max(value))
+
+n = int(stdin.readline())
+arr = list(map(int, stdin.readline().split()))
+arr.insert(0, -1)
+n = int(stdin.readline())
+for _ in range(n):
+    sex, idx = map(int, stdin.readline().split())
+    if sex == 1:
+        for idx in range(idx, len(arr), idx):
+            arr[idx] = (arr[idx] + 1) % 2
+    elif sex == 2:
+        width = find_symmetric(arr, idx)
+        temp = arr[idx-width: idx+width + 1]
+        for i in range(len(temp)):
+            temp[i] = (temp[i] + 1) % 2
+        arr[idx-width:idx+width+1] = temp
+for idx in range(1, len(arr), SPLITS):
+    print(*arr[idx:idx+SPLITS])
