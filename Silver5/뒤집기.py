c3b8bdff886bfb28958f1d211b046b553312516e
JunSeong <kjs35323@naver.com>
2021-02-07 16:23:15 +0900

    Day-37 fix

--- Silver5/뒤집기.py
+++ Silver5/뒤집기.py
@@ -2,16 +2,21 @@
 # 그리디
 from sys import stdin
 
-s = stdin.readline()
-change = []
-for i in range(1, len(s)):
-    if s[i - 1] != s[i]:
-        change.append(i)
-isOdd = False
-if len(change) % 2 == 1:
-    isOdd = True
+S = stdin.readline().rstrip()
+m = '0' if S.count('0') < S.count('1') else '1'
+cnt1 = 0
+c = S[0]
+cnt2 = 0
+for i in range(1, len(S)):
+    if S[i] != c and S[i] == '0':
+        cnt1 += 1
+    elif S[i] != c and S[i] == '1':
+        cnt2 += 1
+    c = S[i]
+if S[-1] == '1':
+    cnt1 += 1
+else:
+    cnt2 += 1
+print(cnt1) if cnt1 < cnt2 else print(cnt2)
 
-result = len(change) // 2
-if isOdd:
-    result += 1
-print(result)
+
