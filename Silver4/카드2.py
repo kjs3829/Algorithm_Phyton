f8af31494c6f5f522a0ff6eb47f0c89766af2012
JSK <kjs35323@naver.com>
2021-02-11 20:16:14 +0900

    Day-43

---
+++ Silver4/카드2.py
@@ -0,0 +1,12 @@
+# BOJ 2164 카드2
+# 큐
+import sys
+from collections import deque
+N = int(sys.stdin.readline())
+queue = deque()
+for i in range(N):
+    queue.append(i + 1)
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+print(queue.pop())
