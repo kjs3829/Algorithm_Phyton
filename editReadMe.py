322f5978f3567a717cae7e966e07d28a268f0860
JunSeong <kjs35323@naver.com>
2021-02-17 23:57:59 +0900

    Add editReadMe.py

---
+++ editReadMe.py
@@ -0,0 +1,23 @@
+
+def sentence(p1, p2):
+    return '<br />[BOJ '+ p1 + '](https://www.acmicpc.net/problem/' + p1 + ')<br />[BOJ ' + p2 + '](https://www.acmicpc.net/problem/' + p2 + ')'
+
+
+n = input("Enter YYYY M D to edit README.md : ").split()
+p1, p2 = input("Enter the number of problem1 problem2 : ").split()
+flag = False
+buffer = ''
+with open("README.md", 'r+', encoding='utf-8') as fin:
+    data = fin.readlines()
+    for line in data:
+        if line == '<!--' + n[0] + ' ' + n[1] + '-->\n':
+            flag = True
+        if flag:
+            if line == '</p>\n':
+                flag = False
+            buffer += line.replace('**' + n[2] + '**', '**' + n[2] + '**' + sentence(p1, p2))
+        else:
+            buffer += line
+with open("README.md", 'w+', encoding='utf-8') as f:
+    f.write(buffer)
+
