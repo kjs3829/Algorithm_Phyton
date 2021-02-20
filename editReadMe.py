e8b7723954648e0f4420c5914890b481dc6a5ed9
JunSeong <kjs35323@naver.com>
2021-02-20 20:02:39 +0900

    Add 'today' Input Keyword

--- editReadMe.py
+++ editReadMe.py
@@ -1,4 +1,5 @@
 # 날짜(YYYY M D)와 문제 번호 두개(p1 p2)를 입력하면 readme.md를 업데이트 시켜준다.
+# 날짜에 'today'를 입력하면 현재 날자를 적용시킨다.
 from datetime import datetime
 import re
 
@@ -8,6 +9,11 @@ def sentence(p1, p2):
 
 # UserInterface
 n = input("Enter YYYY M D to edit README.md : ").split()
+y, m, d = 0, 0, 0
+if n[0] == 'today':
+    y, m, d = datetime.today().year, datetime.today().month, datetime.today().day
+else:
+    y, m, d = n[0], n[1], n[2]
 p1, p2 = input("Enter the number of problem1 problem2 : ").split()
 flag = False
 buffer = ''
@@ -15,14 +21,14 @@ buffer = ''
 # Service
 with open("README.md", 'r+', encoding='utf-8') as fin:
     data = fin.readlines()
-    p = re.compile('\*\*' + n[2] + '[^\|]*')
+    p = re.compile('\*\*' + str(d) + '[^\|]*')
     for line in data:
-        if line == '<!--' + n[0] + ' ' + n[1] + '-->\n':
+        if line == '<!--' + str(y) + ' ' + str(m) + '-->\n':
             flag = True
         if flag:
             if line == '</p>\n':
                 flag = False
-            buffer += p.sub('**' + n[2] + '**' + sentence(p1, p2), line)
+            buffer += p.sub('**' + str(d) + '**' + sentence(p1, p2), line)
             # - old version
             # 이미 작성된 날짜는 update가 불가능한 코드
             # buffer += line.replace('**' + str(d) + '**', '**' + str(d) + '**' + sentence(p1, p2))
